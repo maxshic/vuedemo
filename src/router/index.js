870a3e1a20424a16b7052f1822d2c01aac394291
最爱滕小厨 <teng520jiao@qq.com>
2018-10-24 18:07:35 +0800

    scss

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'HelloWorld',
+			//component: HelloWorld,
+			//路由懒加载
+			component: resolve => require(['@/components/HelloWorld'] ,resolve)
+		}
+	]
 })
